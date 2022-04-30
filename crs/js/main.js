90c02f2b51e4e1abe7582de140687f9be0edba87
jenishov <adilet.jenishov@mail.ru>
2022-04-30 22:16:09 +0600

    first load

---
+++ crs/js/main.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn__login') .onclick =function () {
+    document.querySelector('.overlay').classList.add('overlay_active')
+}
+
+
+
+document.querySelector('.popup__close') .onclick =function () {
+    document.querySelector('.overlay').classList .remove('overlay_active')
+}
